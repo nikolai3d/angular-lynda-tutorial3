61c967639939907172c3da7a399245fafddf2c77
Nikolai Svakhin <svakhine@gmail.com>
2016-02-06 20:28:01 +0000

    Login/Submit button now displays the welcome message

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -1,7 +1,8 @@
 gApp.controller('RegistrationController', ['$scope', function($scope){
     $scope.message = "Welcome To MyApp";
+    
+    $scope.login = function() {
+        $scope.message = "Welcome " + $scope.user.email;
+    }
 }]);
 
-gApp.controller('SuccessController', ['$scope', function($scope){
-    $scope.message = "Welcome To MyApp";
-}]);
