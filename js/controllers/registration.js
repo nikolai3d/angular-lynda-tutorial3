a2fd0dd47227fdf3a889ef46af4bc18872dfe1b3
Nikolai Svakhin <svakhine@gmail.com>
2016-02-06 20:36:39 +0000

    Registration validation and submit callback in controller hooked up

--- js/controllers/registration.js
+++ js/controllers/registration.js
@@ -4,5 +4,10 @@ gApp.controller('RegistrationController', ['$scope', function($scope){
     $scope.login = function() {
         $scope.message = "Welcome " + $scope.user.email;
     }
+    
+    $scope.register = function() {
+        $scope.message = "Welcome " + $scope.user.firstname;
+    }
+    
 }]);
 
