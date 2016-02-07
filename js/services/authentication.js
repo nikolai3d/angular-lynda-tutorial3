d43fd79754477f9fb1a22b4cd622071a6ae06a1b
Nikolai Svakhin <svakhine@gmail.com>
2016-02-07 06:40:23 +0000

    using AngularFire's  method and  service, get user's data upon a successful authentication

--- js/services/authentication.js
+++ js/services/authentication.js
@@ -2,12 +2,32 @@
 
 gApp.factory('Authentication', ['$rootScope',
     '$firebaseAuth',
+    '$firebaseObject',
     '$location',
     'FIREBASE_URL',
-    function($rootScope, $firebaseAuth, $location, FIREBASE_URL) {
+    function($rootScope, $firebaseAuth, $firebaseObject, $location, FIREBASE_URL) {
 
         var fbRef = new Firebase(FIREBASE_URL);
         var fbAuth = $firebaseAuth(fbRef);
+
+        fbAuth.$onAuth(function(fbUserReg){
+            //fbUserReg is whatever comes from successful authentication.
+            //We are using its uid to query name/email etc from our custom data ("users") database.
+            if (fbUserReg){
+                var fbUserRef = new Firebase(FIREBASE_URL+"users/"+fbUserReg.uid);
+                
+// The $firebaseObject service takes a Firebase reference or Firebase Query and returns 
+// a JavaScript object which contains the data at the provided Firebase reference and some extra 
+// AngularFire-specific fields. 
+// Note that the data will not be available immediately since retrieving it is an asynchronous operation. 
+// You can use the $loaded() promise to get notified when the data has loaded.
+// From https://www.firebase.com/docs/web/libraries/angular/api.html#angularfire-firebaseobject   
+                var userObj = $firebaseObject(fbUserRef);
+                $rootScope.currentUser = userObj;
+            } else {
+                $rootScope.currentUser = "";
+            }
+        });
 
         return {
             login: function(user) {
@@ -25,7 +45,12 @@ gApp.factory('Authentication', ['$rootScope',
 
                 $rootScope.message = "Authenticating...";
 
-            },
+            }, //login
+            
+            logout: function() {
+                return fbAuth.$unauth();
+            }, //logout 
+            
             register: function(user) {
                 var fbPromise = fbAuth.$createUser({
                     email: user.email,
